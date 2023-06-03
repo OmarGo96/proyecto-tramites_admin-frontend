d7ed4d85c31a7d4635a15b4f0fe2c3f18a22ac4a
Omar Gonzalez <omargonzalez9911@yahoo.com>
2023-06-03 11:09:35 -0500

    Se cambia en donde apunta sistema de tramites

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  urlApi: 'http://189.240.200.7:8028/api', 
+  /** Actualmente donde esta DSA */
+  //urlApi: 'http://189.240.200.7:8028/api', 
+  /** Actualmente servidor dedicado a tramites */
+  urlApi: 'http://187.175.225.67:8028/api'
 };
