d13230a077a9c9c67d5b1cfb42ff7911ceb1984d
Omar Gonzalez <omargonzalez9911@yahoo.com>
2025-02-20 18:57:24 -0500

    Se adjunta nueva ruta del back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   //urlApi: 'http://189.240.200.7:8028/api',
   /** Actualmente servidor dedicado a tramites */
   //urlApi: 'http://187.175.225.67:8028/api'
-  urlApi: 'https://tramitesyservicios.gobiernodesolidaridad.gob.mx:8028/api'
+  urlApi: 'https://tramitesyservicios.gobiernodesolidaridad.gob.mx/service/api'
 };
