67883078b1345f8837bd1c61f64e5b2a9f799e39
Omar Gonzalez <omargonzalez9911@yahoo.com>
2025-12-31 19:57:16 -0500

    Se adjunta cambio en la URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   //urlApi: 'http://189.240.200.7:8028/api',
   /** Actualmente servidor dedicado a tramites */
   //urlApi: 'http://187.175.225.67:8028/api'
-  //urlApi: 'https://tramitesyservicios.playadelcarmen.gob.mx/service/api'
-  urlApi: 'http://201.98.122.133:8002/api'
+  urlApi: 'https://tramitesyservicios.playadelcarmen.gob.mx/service/api'
+  //urlApi: 'http://201.98.122.133:8002/api'
 };
